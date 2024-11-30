726c86be66a950dbb96798b080d6d465aa8d7cbb
min-jp <mincjiip@gmail.com>
2024-12-01 01:37:12 +0900

    slower, pause behave init code ability 이미지 추가

---
+++ src/engine/src/rabbitescape/engine/behaviours/Pause.java
@@ -0,0 +1,37 @@
+package rabbitescape.engine.behaviours;
+
+import rabbitescape.engine.*;
+
+public class Pause extends Behaviour
+{
+    @Override
+    public ChangeDescription.State newState(
+        BehaviourTools t,
+        boolean triggered
+    )
+    {
+        return null;
+    }
+
+    @Override
+    public boolean behave(
+        World world,
+        Rabbit rabbit,
+        ChangeDescription.State state
+    )
+    {
+        return false;
+    }
+
+    @Override
+    public boolean checkTriggered( Rabbit rabbit, World world )
+    {
+        return false;
+    }
+
+    @Override
+    public void cancel()
+    {
+
+    }
+}
